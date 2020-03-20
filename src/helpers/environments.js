281d2cada4be6726227898e729e1ca6f4915ced6
Chris Kraft <kriggity@gmail.com>
2020-03-20 14:06:52 -0400

    changed heroku case in environments.js

--- src/helpers/environments.js
+++ src/helpers/environments.js
@@ -5,7 +5,7 @@ switch (window.location.hostname) {
     case 'localhost' || '127.0.0.1':
         APIURL = 'http://localhost:3001'
         break;
-    case 'cmk-bluebadge-server.herokuapp.com/':
+    case 'cmk-bluebadge-client.herokuapp.com/':
         APIURL = 'https://cmk-bluebadge-server.herokuapp.com/'
 }
 export default APIURL;
