58dd8dc2ca57a35831564e557dfd0d1dce59c6b5
Chris Kraft <kriggity@gmail.com>
2020-03-22 18:21:40 -0400

    troubleshooting login APIURL

--- src/helpers/environments.js
+++ src/helpers/environments.js
@@ -4,8 +4,8 @@ switch (window.location.hostname) {
     case 'localhost' || '127.0.0.1':
         APIURL = 'http://localhost:3001'
         break;
-    case 'cmk-bluebadge-client.herokuapp.com/':
-        APIURL = 'https://cmk-bluebadge-server.herokuapp.com/'
+    case 'cmk-bluebadge-client.herokuapp.com':
+        APIURL = 'https://cmk-bluebadge-server.herokuapp.com'
 }
 console.log(`window.location.hostname ${window.location.hostname }`);
 console.log(`APIURL${APIURL}`);
